f58d41dba84705ebc7863f4be94bc047924c75ef
Violeta Georgieva <vgeorgieva@pivotal.io>
2019-05-15 18:04:27 +0200

    Switch on Reactor Debug agent

--- frontend/src/main/java/io/spring/sample/demo/DemoApplication.java
+++ frontend/src/main/java/io/spring/sample/demo/DemoApplication.java
@@ -2,13 +2,13 @@ package io.spring.sample.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Hooks;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		Hooks.onOperatorDebug();
+		ReactorDebugAgent.init();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
