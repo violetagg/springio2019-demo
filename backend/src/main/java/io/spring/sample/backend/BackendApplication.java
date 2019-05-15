1f6f57cdedba76ebc6a18e60ceab88e1fe650d79
Violeta Georgieva <vgeorgieva@pivotal.io>
2019-05-15 18:04:08 +0200

    Switch on Hooks#onOperatorDebug

--- backend/src/main/java/io/spring/sample/backend/BackendApplication.java
+++ backend/src/main/java/io/spring/sample/backend/BackendApplication.java
@@ -2,11 +2,13 @@ package io.spring.sample.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Hooks.onOperatorDebug();
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
--- frontend/src/main/java/io/spring/sample/demo/DemoApplication.java
+++ frontend/src/main/java/io/spring/sample/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package io.spring.sample.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Hooks.onOperatorDebug();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
